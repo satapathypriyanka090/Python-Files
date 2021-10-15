40fe7fed0d4e1ae3086978c9572d2bc6ca9908e2
singhaniaveer <92604186+singhaniaveer@users.noreply.github.com>
2021-10-16 01:57:45 +0530

    Added lists.py

---
+++ lists.py
@@ -0,0 +1,54 @@
+import os.path
+import json
+from datetime import datetime
+
+FILE_NAME = 'lists.json'
+
+def show_lists(args):
+  with open(FILE_NAME, 'r') as lists_json:
+    try:
+      data = json.load(lists_json)
+      for index, todo_list in enumerate(data.keys()):
+        print(index + 1, data[todo_list]['title'])
+    except:
+      print('Some error occurred!1')
+
+def use_list(args):
+  list_name = args[0]
+  with open(FILE_NAME, 'r') as lists_json:
+    try:
+      data = json.load(lists_json)
+      if (data.get(list_name)):
+        return f'{list_name}.json'
+      else:
+        return -1
+    except:
+      print('Some error occurred!2')
+
+def create_list(args):
+  list_name = args[0]
+  # print(os.path.abspath('.'))
+  new_list = {}
+  with open(FILE_NAME, 'r+') as lists_json:
+    try:
+      data = json.load(lists_json)
+      # print(data)
+      # check if file already exists
+      if (data.get(list_name)):
+        print('List already exists! Try a different name...')
+      else:
+        # update the new_list dict
+        new_list = {
+        'title': list_name,
+        'created_at': datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        }
+        data[list_name] = new_list
+        with open(f'{list_name}.json', 'w') as new_list:
+          # empty list
+          new_list.write('[\n]')
+          print('Successfully created the new list!')
+        # add to the lists.json
+        lists_json.seek(0)
+        json.dump(data, lists_json, sort_keys=True, indent=True)
+    except:
+      print('Some error occurred!3')
